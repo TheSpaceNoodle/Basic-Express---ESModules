c6451581133a8301377d96c434b51efc51042ebe
TheSpaceNoodle <aknojlogiya@gmail.com>
2024-08-21 11:30:51 +0300

    fix: forgot to add json parser middleware

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (_, res) => {
   res.send('Hello World!');
 });
